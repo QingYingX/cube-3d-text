d0ae72fc7717bd982433a63de24850a2051561b5
boybook <fangyizhou1997@qq.com>
2024-12-27 19:45:48 +0800

    尝试修复一个手机端的ui问题

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ const App: React.FC = () => {
         localStorage.setItem("customFonts", JSON.stringify(customFonts));
     }, [customFonts]);
 
-    const [isMobile, setIsMobile] = useState(false);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
     useEffect(() => {
         const handleResize = () => {
