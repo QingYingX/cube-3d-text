8aa0c15c42ec0b8185dd464c5ad22775d6aa5e99
QingYing_X <118583665+QingYingX@users.noreply.github.com>
2025-10-06 19:48:23 +0800

    测试增加追踪代码

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,22 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'inject-umami',
+      transformIndexHtml(html) {
+        return html.replace(
+          '</head>',
+          `  <script 
+            defer 
+            src="https://umami.099311.xyz/script.js" 
+            data-website-id="c524f440-7c7f-4e16-8145-4c534d10a6ed"
+          ></script>
+</head>`
+        )
+      }
+    }
+  ],
   server: { port: 3000 }
 })
